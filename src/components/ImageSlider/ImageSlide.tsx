29f6d86cc8e4c388f4d86b30900b2ed594d326a3
Nicolás Rodrígues Chaves <nicolasrodriguezch@hotmail.com>
2025-02-04 12:01:09 -0500

    adds styles

--- src/components/ImageSlider/ImageSlide.tsx
+++ src/components/ImageSlider/ImageSlide.tsx
@@ -2,8 +2,16 @@ import { ImageSlideProps } from '../../utils/types/Types';
 
 export const ImageSlide = ({ isActive, src }: ImageSlideProps) => {
   return (
-    <div style={{ display: isActive ? 'block' : 'none' }}>
-      <img src={src} alt='Slide' />
+    <div
+      className={`absolute inset-0 m-4 transition-opacity duration-700 flex items-center justify-center ${
+        isActive ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <img
+        src={src}
+        alt='Slide'
+        className='max-w-full max-h-full object-contain rounded-2xl'
+      />
     </div>
   );
 };
--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -12,17 +12,30 @@ export const ImageSlider = () => {
   const goNext = () => {
     setActiveIndex((prev) => (prev + 1) % imageArray.length);
   };
+
   const goPrevious = () => {
     setActiveIndex((prev) => (prev === 0 ? imageArray.length - 1 : prev - 1));
   };
 
   return (
-    <div>
-      <button onClick={goPrevious}>Previous</button>
+    <div className='relative w-full h-64 sm:h-80 lg:h-96 overflow-hidden flex items-center justify-center'>
+      <button
+        onClick={goPrevious}
+        className='absolute cursor-pointer left-2 sm:left-4 bg-black bg-opacity-50 text-white p-2 sm:p-3 rounded-full hover:bg-opacity-75 focus:outline-none z-10'
+      >
+        &#10094; {}
+      </button>
+
       {imageArray.map((image, index) => (
         <ImageSlide key={index} src={image} isActive={index === activeIndex} />
       ))}
-      <button onClick={goNext}>Next</button>
+
+      <button
+        onClick={goNext}
+        className='absolute cursor-pointer right-2 sm:right-4 bg-black bg-opacity-50 text-white p-2 sm:p-3 rounded-full hover:bg-opacity-75 focus:outline-none z-10'
+      >
+        &#10095; {}
+      </button>
     </div>
   );
 };
