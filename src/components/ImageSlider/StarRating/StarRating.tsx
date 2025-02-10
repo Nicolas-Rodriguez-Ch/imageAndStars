f392d9bd96e96340c0e74f74673405255615e5f5
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-02-10 09:54:44 -0500

    starts creating rating system

--- src/components/ImageSlider/StarRating/StarRating.tsx
+++ src/components/ImageSlider/StarRating/StarRating.tsx
@@ -5,12 +5,26 @@ export const StarRating = () => {
   const filledStar = '★';
 
   const [selectedStars, setSelectedStars] = useState(0);
-  const starArray = new Array(5).fill(emptyStar, selectedStars);
+  const handleClick = (index: number) => {
+    setSelectedStars(index + 1);
+  };
+
   return (
-    <div className='flex gap-4'>
-      {starArray.map((star) => (
-        <h1 key={star}>{star}</h1>
-      ))}
+    <div className='flex flex-col text-center'>
+      <h1 className='mb-2 text-lg font-semibold'>Rate this picture</h1>
+      <div className='flex gap-4'>
+        {new Array(5).fill(null).map((_, index) => (
+          <span
+            key={index}
+            className={`cursor-pointer text-4xl ${
+              index < selectedStars ? 'text-yellow-500' : 'text-gray-400'
+            }`}
+            onClick={() => handleClick(index)}
+          >
+            {index < selectedStars ? filledStar : emptyStar}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
